249e3ac25326196e8d5a4b6093915b9e190c6688
changlor <958142428@qq.com>
2016-03-20 19:54:36 +0800

    加入对分页参数传入错误数据时的处理

--- app/controllers/Topic.php
+++ app/controllers/Topic.php
@@ -24,6 +24,9 @@ class Topic extends Base
         $pagination_rows = 6;
         $page_rows = ceil($topic['comment_count'] / $pagination_rows);
         $p = $this->request->input('get.p');
+        if (!is_numeric($p) || $p < 1 || $p > $page_rows) {
+            $p = '';
+        }
         $current_page = $p == '' ? $page_rows : $p;
         $comment = $this->model->Comment->getTopicComment($topic_id, $current_page, $pagination_rows);
         $page = new Page($topic['comment_count'], $pagination_rows);
